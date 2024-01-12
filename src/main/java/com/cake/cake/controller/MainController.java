695536e4bd4d47d6666fb29967b53a08497421df
John Doe <hassan.nazamul05@gmail.com>
2024-01-12 01:28:36 -0500

    All good

--- src/main/java/com/cake/cake/controller/MainController.java
+++ src/main/java/com/cake/cake/controller/MainController.java
@@ -1,13 +1,21 @@
 package com.cake.cake.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.cake.cake.data.UserRepository;
 import com.cake.cake.model.Users;
 
 @Controller
 public class MainController {
+
+    // Got the object of UserRepository class
+    @Autowired
+    UserRepository userRepository;
 
     // Home/landing page
     @RequestMapping("/")
@@ -29,6 +37,14 @@ public class MainController {
     public String register(Model model) {
         model.addAttribute("user", new Users());
         return "register";
+    }
+
+    // debugging purpose
+    @ResponseBody
+    @RequestMapping("/signUpDone")
+    public Users signUpDone(Model model, @ModelAttribute Users user) {
+        userRepository.save(user);
+        return user;
     }
 
 }
