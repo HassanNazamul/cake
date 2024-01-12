af77f62f94199dc8551f5850527ba70e318ffa98
John Doe <hassan.nazamul05@gmail.com>
2024-01-12 00:06:52 -0500

    Updated Login and Register Page

--- src/main/java/com/cake/cake/controller/MainController.java
+++ src/main/java/com/cake/cake/controller/MainController.java
@@ -6,8 +6,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
+    // Home/landing page
     @RequestMapping("/")
     public String index() {
         return "index";
     }
+
+    // login page and button is available on the home page Nav bar
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    // register page and link is available on the login page (line:89)
+    @RequestMapping("/register")
+    public String register() {
+        return "register";
+    }
+
 }
