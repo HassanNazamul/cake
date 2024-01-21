3c934273faf15a74df3ee40e36fcffa91ad6b0bb
John Doe <hassan.nazamul05@gmail.com>
2024-01-20 23:26:03 -0500

    all good now ill add security

--- src/main/java/com/cake/cake/controller/MainController.java
+++ src/main/java/com/cake/cake/controller/MainController.java
@@ -1,13 +1,9 @@
 package com.cake.cake.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.cake.cake.data.UserRepository;
 import com.cake.cake.model.Users;
